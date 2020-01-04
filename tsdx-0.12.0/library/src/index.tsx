c2ad545114809dceae0643e60878180d8525405e
Florian Knop <florian_knop@hotmail.com>
2020-01-04 18:18:56 +0100

    initial commit

---
+++ tsdx-0.12.0/library/src/index.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+
+// Delete me
+export const Comp = styled.div`
+  ${tw`text-xs`}
+`
---
+++ tsdx-0.12.0/library/src/tailwind.macro.d.ts
@@ -0,0 +1,4 @@
+declare module 'tailwind.macro' {
+  let tw: (value: TemplateStringsArray) => any
+  export default tw
+}
